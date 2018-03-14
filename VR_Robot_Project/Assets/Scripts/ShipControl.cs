91b42460b9f1e5457bcecc7ab965d7e6630ca8b0
Kenjiro Goodson <kcgoodson@gmail.com>
2018-03-13 20:45:14 -0700

    Need to Fix Foward and Back

--- VR_Robot_Project/Assets/Scripts/ShipControl.cs
+++ VR_Robot_Project/Assets/Scripts/ShipControl.cs
@@ -5,8 +5,8 @@ using UnityEngine.UI;
 
 public class ShipControl : MonoBehaviour {
 
-	//Vector3[] directions = new Vector3[]{Vector3.forward, Vector3.left, Vector3.back, Vector3.right, Vector3.up, Vector3.down};
-	//KeyCode[] keys = new KeyCode[]{KeyCode.I, KeyCode.J, KeyCode.K, KeyCode.L, KeyCode.U, KeyCode.O};
+	Vector3[] directions = new Vector3[]{Vector3.forward, Vector3.left, Vector3.back, Vector3.right, Vector3.up, Vector3.down};
+	KeyCode[] keys = new KeyCode[]{KeyCode.I, KeyCode.J, KeyCode.K, KeyCode.L, KeyCode.U, KeyCode.O};
 	public float speed = 30;
 	//public float buffer = 0f;
 	//public int player;
@@ -48,15 +48,8 @@ public class ShipControl : MonoBehaviour {
 		float sumY = leftV.y + rightV.y;
 
 
-		//Vector3 myForward = head.transform.forward.normalized;
-		//Vector3 myRight = head.transform.right.normalized;
 
-		sum = head.transform.forward.normalized;
-		sum = new Vector3 (sum.x * sumX, sumVertical, sum.z * sumY);
-
-
-		//sum = myForward * sumX + myRight * sumY;
-		//sum = new Vector3 (sum.x, sumVertical, -sum.z);
+		sum = myForward + myRight;
 
 		//bugger.text = "SUMX: " + sumX + "\nSUMY: " + sumY;
 		transform.Translate (sum * speed * Time.deltaTime);
