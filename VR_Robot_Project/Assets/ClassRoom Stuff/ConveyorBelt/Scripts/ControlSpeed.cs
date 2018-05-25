2b3e72eb9ee50462da61151fae6634c592688fb3
donglidesigner <philoshearing@hotmail.com>
2018-05-24 23:22:07 -0700

    Update UI, enable switching expressions and stop conveyor belt
    Keyboard shortcuts:
    R = Reload scene
    M = Diable conveyor belt
    B, N = Switch expressions

---
+++ VR_Robot_Project/Assets/ClassRoom Stuff/ConveyorBelt/Scripts/ControlSpeed.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlSpeed : MonoBehaviour {
+	ConveyorScript myConveyorScript;
+	// Use this for initialization
+	void Start () {
+		myConveyorScript = GetComponent<ConveyorScript>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.M))
+		myConveyorScript.enabled = !myConveyorScript.enabled;
+	}
+}
---
+++ VR_Robot_Project/Assets/Scripts/SwitchExpression.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchExpression : MonoBehaviour {
+	public Material[] myMaterials;
+	//public GameObject Object;
+	private int count = 0;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetKeyDown(KeyCode.N)){
+			count ++;
+			if(count >= myMaterials.Length){
+				count = 0;
+			}
+		}
+		if(Input.GetKeyDown(KeyCode.B)){
+			count --;
+			if(count < 0){
+				count = myMaterials.Length - 1;
+			}
+		}
+
+		gameObject.transform.GetChild(0).GetChild(0).GetComponent<MeshRenderer>().material = myMaterials[count];
+	}
+}
