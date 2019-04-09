812412a8acf74a5ff8bc1a83c22b749fdffdd4f9
simran18 <simran18@uw.edu>
2019-04-09 00:04:30 -0700

    Add face and EMAR V4

--- VR_Robot_Project/Assets/Scripts/RobotControl.cs
+++ VR_Robot_Project/Assets/Scripts/RobotControl.cs
@@ -2,51 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RobotControl : MonoBehaviour {
+public class RobotControl : MonoBehaviour
+{
 
-	Rigidbody myBody;
-	public float rotateSpeed;
-	public float speed;
-	public string[] keys;
+    Rigidbody myBody;
+    public float speed;
 
-	void Start() {
-		myBody = GetComponent<Rigidbody> ();
-		keys = new string[5];
-		for (int i = 0; i < keys.Length; i++) {
-			keys [i] = "player1_" + i;
-		}
-	}
 
-	// Update is called once per frame
-	void Update () {
+    void Start()
+    {
+        myBody = GetComponent<Rigidbody>();
 
-		//Controls Forward and Backward Movement
-		int direction = 0;
-		if (Input.GetButton (keys [2])) {
-			direction = -1;
-		} else if (Input.GetButton (keys [0])) {
-			direction = 1;
-		}
-		if (direction != 0) {
-			Vector3 newDirection = (transform.forward * speed * direction);
-			newDirection.y = myBody.velocity.y;
-			myBody.velocity = newDirection;
-			//myBody.AddForce(transform.forward*speed*direction*myBody.mass);
-		}
+    }
 
-		//Controls Rotation
-		direction = 0;
-		if (Input.GetButton (keys [1])) {
-			direction = -1;
-		} else if (Input.GetButton (keys [3])) {
-			direction = 1;
-		}
-		myBody.angularVelocity = rotateSpeed * direction * transform.up;
-		transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
-		//Plays AudioClip
-		if(Input.GetButtonDown(keys[4])) {
-			GetComponent<AudioSource> ().Play ();
-		}
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Vector3 position = myBody.transform.position;
+            position.x--;
+            myBody.transform.position = position;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Vector3 position = myBody.transform.position;
+            position.x++;
+            myBody.transform.position = position;
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Vector3 position = myBody.transform.position;
+            position.y++;
+            myBody.transform.position = position;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Vector3 position = myBody.transform.position;
+            position.y--;
+            myBody.transform.position = position;
+        }
+    }
 
-	}
-}
+    }
+   
