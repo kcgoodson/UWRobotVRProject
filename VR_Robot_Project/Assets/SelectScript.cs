974645424b29b890f89abc02d82a0d0562835b97
CoreyS <coreyseaman@gmail.com>
2019-04-29 18:57:48 -0700

    Added Voice Buttons

---
+++ VR_Robot_Project/Assets/SelectScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectScript : MonoBehaviour
+{
+    public Button button;
+    public Button button2;
+    void Start()
+    {
+        button.Select();
+        button2.Select();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ VR_Robot_Project/Assets/Voices.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Voices : MonoBehaviour
+{
+    public void test()
+    {
+        Debug.Log("Clicked");
+    }
+
+    public AudioClip MusicClip;
+
+    public AudioSource MusicSource;
+
+    void Start()
+    {
+        MusicSource.clip = MusicClip;
+    }
+
+    public void Play()
+    {
+        MusicSource.Play();
+    }
+}
