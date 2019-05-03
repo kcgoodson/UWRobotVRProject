8ee8ee84b72de36b02c5aa1a8c0643f13acd0b1f
CoreyS <coreyseaman@gmail.com>
2019-05-02 20:01:38 -0700

    Merged Highlighting text and writing

--- VR_Robot_Project/Assets/InputFieldScript.cs
+++ VR_Robot_Project/Assets/InputFieldScript.cs
@@ -12,28 +12,62 @@ public class InputFieldScript : MonoBehaviour
     string operatorData;
     string participantData;
 
-    private void Start()
+    public void Setget()
     {
-        this.startSession = GetComponent<Button>();
-        this.endSession = GetComponent<Button>();
-    }
-
-    public void Setget() {
         this.operatorData = operatorInput.text.ToString();
         this.participantData = participantInput.text.ToString();
-        WriteString(operatorData, participantData);
+        WriteInitial(operatorData, participantData);
     }
 
-    static void WriteString(string inputO, string inputP){
+    static void WriteInitial(string inputO, string inputP)
+    {
         string path = "Assets/Resources/data.txt";
 
-        //Write some text to the test.txt file
+        //Write some text to the data.txt file
         StreamWriter writer = new StreamWriter(path, true);
         writer.WriteLine("Operator-Name:" + inputO + ", Participant-ID:" + inputP);
         writer.Close();
 
         //Re-import the file to update the reference in the editor
-        AssetDatabase.ImportAsset(path);
-        TextAsset asset = (TextAsset) Resources.Load("data");
+        Resources.Load(path);
+        //AssetDatabase.Import(path);
+        TextAsset asset = (TextAsset)Resources.Load("data.txt");
     }
+
+    public void OnClickCount(Button clickedButton)
+    {
+        string path = "Assets/Resources/data.txt";
+
+        //Write some text to the data.txt file
+        StreamWriter writer = new StreamWriter(path, true);
+        writer.WriteLine("Button: " + clickedButton.name + " Click: 1");
+        writer.Close();
+
+        //Re-import the file to update the reference in the editor
+        Resources.Load(path);
+        //AssetDatabase.ImportAsset(path);
+        TextAsset asset = (TextAsset)Resources.Load("data");
+
+    }
+
+    public void EndSessioWrite()
+    {
+        string path = "Assets/Resources/data.txt";
+
+        //Write some text to the data.txt file
+        StreamWriter writer = new StreamWriter(path, true);
+        writer.WriteLine("Session Ended --");
+        writer.Close();
+
+        //Re-import the file to update the reference in the editor
+        Resources.Load(path);
+        //AssetDatabase.ImportAsset(path);
+        TextAsset asset = (TextAsset)Resources.Load("data");
+
+        operatorInput.ActivateInputField();
+        startSession.interactable = true;
+        endSession.interactable = true;
+    }
+
+
 }
