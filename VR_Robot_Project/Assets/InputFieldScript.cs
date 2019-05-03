7fe70efaee02d9d6fe22262268937ac3125f44e9
simran18 <simran18@uw.edu>
2019-05-02 18:09:29 -0700

    Add end session button to work

--- VR_Robot_Project/Assets/InputFieldScript.cs
+++ VR_Robot_Project/Assets/InputFieldScript.cs
@@ -12,22 +12,16 @@ public class InputFieldScript : MonoBehaviour
     string operatorData;
     string participantData;
 
-    private void Start()
-    {
-        this.startSession = GetComponent<Button>();
-        this.endSession = GetComponent<Button>();
-    }
-
     public void Setget() {
         this.operatorData = operatorInput.text.ToString();
         this.participantData = participantInput.text.ToString();
-        WriteString(operatorData, participantData);
+        WriteInitial(operatorData, participantData);
     }
 
-    static void WriteString(string inputO, string inputP){
+    static void WriteInitial(string inputO, string inputP){
         string path = "Assets/Resources/data.txt";
 
-        //Write some text to the test.txt file
+        //Write some text to the data.txt file
         StreamWriter writer = new StreamWriter(path, true);
         writer.WriteLine("Operator-Name:" + inputO + ", Participant-ID:" + inputP);
         writer.Close();
@@ -36,4 +30,39 @@ public class InputFieldScript : MonoBehaviour
         AssetDatabase.ImportAsset(path);
         TextAsset asset = (TextAsset) Resources.Load("data");
     }
+
+    public void OnClickCount(Button clickedButton)
+    {
+        string path = "Assets/Resources/data.txt";
+
+        //Write some text to the data.txt file
+        StreamWriter writer = new StreamWriter(path, true);
+        writer.WriteLine("Button: " + clickedButton.name + " Click: 1");
+        writer.Close();
+
+        //Re-import the file to update the reference in the editor
+        AssetDatabase.ImportAsset(path);
+        TextAsset asset = (TextAsset)Resources.Load("data");
+
+    }
+
+    public void EndSessioWrite()
+    {
+        string path = "Assets/Resources/data.txt";
+
+        //Write some text to the data.txt file
+        StreamWriter writer = new StreamWriter(path, true);
+        writer.WriteLine("Session Ended --");
+        writer.Close();
+
+        //Re-import the file to update the reference in the editor
+        AssetDatabase.ImportAsset(path);
+        TextAsset asset = (TextAsset)Resources.Load("data");
+
+        operatorInput.ActivateInputField();
+        startSession.interactable = true;
+        endSession.interactable = true;
+    }
+
+
 }
